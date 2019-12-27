2f8c73e4d774ef03dbc855125695cd8c584f1f33
Clinton James <clintjeez@gmail.com>
2019-12-27 10:19:36 +0100

    js code structure and general setup

--- app.js
+++ app.js
@@ -10,3 +10,26 @@ const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
 
+// cart implementation
+let cart = [];
+
+// getting the products data.....
+class Products {
+
+}
+
+// display products fetching the products data with logic
+class UI {
+
+}
+
+// Local storage
+class Storage {
+
+}
+
+// events trigger calling functions and methods
+document.addEventListener('DOMContentLoaded', () => {
+  const ui = new UI()
+  const products = new Products()
+});
